48974a3c7911b09b13abb304e3caa0e20498705f
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-08-24 20:12:40 -0500

    added a few buttons in tool bar

--- src/routes/Desktop/index.tsx
+++ src/routes/Desktop/index.tsx
@@ -3,12 +3,16 @@ import ContextBar from 'components/ContextBar';
 import ToolBar from 'components/ToolBar';
 
 import { ReactComponent as SaveIcon } from 'assets/icons/save.svg';
+import { ReactComponent as UndoIcon } from 'assets/icons/undo.svg';
+import { ReactComponent as RedoIcon } from 'assets/icons/redo.svg';
+import { ReactComponent as ZoomInIcon } from 'assets/icons/zoom-in.svg';
+import { ReactComponent as ZoomOutIcon } from 'assets/icons/zoom-out.svg';
 
 function Desktop() {
     return (
         <PageRoot>
             <ContextBar.Container>
-                <ContextBar.Button maxWidth={true}>File</ContextBar.Button>
+                <ContextBar.Button>File</ContextBar.Button>
                 <ContextBar.Button>Edit</ContextBar.Button>
                 <ContextBar.Button>Selection</ContextBar.Button>
                 <ContextBar.Button>View</ContextBar.Button>
@@ -16,6 +20,12 @@ function Desktop() {
             </ContextBar.Container>
             <ToolBar.Container>
                 <ToolBar.Button><SaveIcon /></ToolBar.Button>
+                <ToolBar.Button><UndoIcon /></ToolBar.Button>
+                <ToolBar.Button><RedoIcon /></ToolBar.Button>
+                
+                <ToolBar.Seperator />
+                <ToolBar.Button><ZoomInIcon /></ToolBar.Button>
+                <ToolBar.Button><ZoomOutIcon /></ToolBar.Button>
             </ToolBar.Container>
         </PageRoot>
     );
