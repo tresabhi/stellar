3b930da7cde08382d8e2a4d68686c1493047a495
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-08-30 12:57:01 -0500

    added temporary version switcher

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,5 +1,19 @@
+import { BrowserView, MobileView } from 'react-device-detect';
+
+import PseudoRoot from 'components/PseudoRoot';
+
 const Home = () => {
-  return <a />;
+  return (
+    <PseudoRoot>
+      <BrowserView>
+        <a href="Desktop">Desktop</a>
+        <br />
+      </BrowserView>
+      <MobileView>
+        <a href="Mobile">Mobile</a>
+      </MobileView>
+    </PseudoRoot>
+  );
 };
 
 export default Home;
