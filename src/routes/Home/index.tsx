c26c5027ec67ac776832a105eace90254d1bd407
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-08-30 18:16:06 -0500

    finilized react layout

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -3,14 +3,21 @@ import { isDesktop, isMobile } from 'react-device-detect';
 import PseudoRoot from 'components/PseudoRoot';
 import DeviceChooser from 'components/DeviceChooser';
 
+import { ReactComponent as DesktopIcon } from 'assets/icons/desktop.svg';
+import { ReactComponent as MobileIcon } from 'assets/icons/mobile.svg';
+
 const Home = () => {
   return (
     <PseudoRoot>
       <DeviceChooser.Container>
         <DeviceChooser.Title>Choose Your Device</DeviceChooser.Title>
         <DeviceChooser.List>
-          <DeviceChooser.Card href="Desktop" text="Desktop"></DeviceChooser.Card>
-          <DeviceChooser.Card href="Mobile" text="Mobile"></DeviceChooser.Card>
+          <DeviceChooser.Card href="Desktop" text="Desktop">
+            <DesktopIcon />
+          </DeviceChooser.Card>
+          <DeviceChooser.Card href="Mobile" text="Mobile">
+            <MobileIcon />
+          </DeviceChooser.Card>
         </DeviceChooser.List>
       </DeviceChooser.Container>
     </PseudoRoot>
