df6f47320fb7cc59728e9ee1f666ed3af24330d4
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-08-31 12:47:00 -0500

    renamed all roots to container

---
+++ src/components/PseudoContainer/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const PageContainer: FC = ({ children }) => {
+  return children;
+};
+
+export default PageContainer;
--- src/components/PseudoRoot/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-
-const PageRoot: FC = ({ children }) => {
-  return children;
-};
-
-export default PageRoot;
