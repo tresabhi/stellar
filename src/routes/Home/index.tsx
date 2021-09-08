0e3e4d3901b25a4cb33b38d5c1a12d2c843db4c2
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-09-07 19:18:17 -0500

    updated home route to match previous commit

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,7 @@
 import { isDesktop, isMobile } from 'react-device-detect';
 
 import PseudoContainer from 'components/PseudoContainer';
-import DeviceChooser from 'components/DeviceChooser';
+import DevicePicker from 'components/DevicePicker';
 
 import { ReactComponent as DesktopIcon } from 'assets/icons/desktop.svg';
 import { ReactComponent as MobileIcon } from 'assets/icons/mobile.svg';
@@ -9,17 +9,17 @@ import { ReactComponent as MobileIcon } from 'assets/icons/mobile.svg';
 const Home = () => {
   return (
     <PseudoContainer>
-      <DeviceChooser.Container>
-        <DeviceChooser.Title>Choose Your Editor Version</DeviceChooser.Title>
-        <DeviceChooser.List>
-          <DeviceChooser.Card recomended={isDesktop} href="desktop" text="Desktop">
+      <DevicePicker.Container>
+        <DevicePicker.Title>Choose Your Editor Version</DevicePicker.Title>
+        <DevicePicker.List>
+          <DevicePicker.Card recomended={isDesktop} href="desktop" text="Desktop">
             <DesktopIcon />
-          </DeviceChooser.Card>
-          <DeviceChooser.Card recomended={isMobile} href="mobile" text="Mobile">
+          </DevicePicker.Card>
+          <DevicePicker.Card recomended={isMobile} href="mobile" text="Mobile">
             <MobileIcon />
-          </DeviceChooser.Card>
-        </DeviceChooser.List>
-      </DeviceChooser.Container>
+          </DevicePicker.Card>
+        </DevicePicker.List>
+      </DevicePicker.Container>
     </PseudoContainer>
   );
 };
