946eddcd6c1024b9a4c2d305969effda7b9adf97
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-09-10 21:21:06 -0500

    fixed canvas ref

--- src/components/EditingCanvas/index.tsx
+++ src/components/EditingCanvas/index.tsx
@@ -1,15 +1,15 @@
-import { createRef } from 'react';
+import { useRef, MutableRefObject } from 'react';
 import 'react-dom';
 import * as THREE from 'three';
 
 import './index.scss';
 
 const EditingCanvas = () => {
-  let canvas = createRef();
+  let canvasRef = useRef<HTMLCanvasElement>(null);
 
   const scene = new THREE.Scene();
 
-  return <canvas ref={canvas} className="editing-canvas" />;
+  return <canvas ref={canvasRef} className="editing-canvas" />;
 };
 
 export default EditingCanvas;
