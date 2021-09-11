afffa5677db6a347f0cedd5404608e1db544a844
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2021-09-11 15:39:27 -0500

    added temporary cylinder rendering

--- src/components/EditingCanvas/index.tsx
+++ src/components/EditingCanvas/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { FC, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { Canvas, useFrame } from '@react-three/fiber';
 import 'react-dom';
@@ -6,27 +6,21 @@ import 'react-dom';
 import './index.scss';
 
 const EditingCanvas = () => {
-  function Box(props: JSX.IntrinsicElements['mesh']) {
-    const ref = useRef<THREE.Mesh>(null!);
-    const [hovered, setHover] = useState(false);
-    const [active, setActive] = useState(false);
-    useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+  const TempCyl: FC<JSX.IntrinsicElements.mesh> = ({ position, height }) => {
     return (
-      <mesh {...props} ref={ref} scale={active ? 1.5 : 1} onClick={(event) => setActive(!active)} onPointerOver={(event) => setHover(true)} onPointerOut={(event) => setHover(false)}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <mesh position={position}>
+        <cylinderGeometry args={[2, 2, height, 24]} />
+        <meshStandardMaterial roughness={0.2} metalness={0.2} flatShading={true} />
       </mesh>
     );
-  }
-
-  // let canvasRef = useRef<HTMLCanvasElement>(null);
+  };
 
   return (
-    <Canvas className="editing-canvas">
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+    <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 100] }} className="editing-canvas">
+      <TempCyl position={[-2, 0, 0]} height={6} />
+      <TempCyl position={[2, 2, 0]} height={2} />
+      <TempCyl position={[4, -3, 0]} height={4} />
+      <directionalLight position={[-0.5, 0, 5]} />
     </Canvas>
   );
 };
