a08b4fa760cf30bdd7210258322e7cfeb2d8d78b
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2021-09-14 18:16:19 -0500

    temporarily made FuelTank useless

--- src/utilities/parts/FuelTank.tsx
+++ src/utilities/parts/FuelTank.tsx
@@ -1,14 +1,5 @@
-interface IFuelTankConstructor {
-  new (height: number, oiginalWidth: number, widthTop: number, widthBottom: void);
-}
-class FuelTank implements IFuelTankConstructor {
-  constructor(height = 2, oiginalWidth = 2, widthTop = 2, widthBottom = 2) {
-    return (
-      <mesh>
-        <cylinderGeometry />
-      </mesh>
-    );
-  }
-}
+const FuelTank = () => {
+  return <></>;
+};
 
 export default FuelTank;
