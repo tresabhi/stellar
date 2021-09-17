7b1c7b5cbdb6406758116bd8cc21e8b27c035bb5
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2021-09-16 20:10:43 -0500

    added infinite grid (man this was a headache)

--- src/components/EditingCanvas/index.tsx
+++ src/components/EditingCanvas/index.tsx
@@ -1,21 +1,25 @@
 import { Canvas } from '@react-three/fiber';
 import 'react-dom';
 import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
 
 import Part from 'components/Part';
+import InfiniteGridHelper from 'components/InfiniteGridHelper';
 
 import './index.scss';
 
 const EditingCanvas = () => {
   return (
-    <Canvas orthographic camera={{ zoom: 32, position: [0, 0, 10] }} className="editing-canvas">
+    <Canvas frameloop={'demand'} orthographic camera={{ zoom: 32, position: [0, 0, 10] }} className="editing-canvas">
       <Part.FuelTank />
+      {/* <Part.FuelTank override={{ N: { width_a: 4 }, p: { x: 4 } }} /> */}
 
       <directionalLight position={[-0, 0, 5]} />
       <ambientLight intensity={0.1} />
 
-      <OrbitControls enablePan={true} enableZoom={true} enableRotate={false} />
-      <gridHelper args={[100, 50, 'green', 'red']} position={[0, 0, -100]} rotation={[(Math.PI / 180) * 90, 0, 0]} />
+      <OrbitControls enableDamping={false} enablePan={true} enableZoom={true} enableRotate={false} />
+
+      <InfiniteGridHelper distance={Math.pow(2, 12)} axes="yxz" size1={1} size2={2} />
     </Canvas>
   );
 };
