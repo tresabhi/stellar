8631338976c2d8d94ba28de48e33bc2c9b847d6e
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2021-09-19 19:22:39 -0500

    switched to workbox-webpack-plugin

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,1 @@
-alert('yolo');
+console.log('test');
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Workbox from 'workbox-webpack-plugin';
 
 import App from './App';
+
+Workbox.register();
 
 ReactDOM.render(
   <React.StrictMode>
