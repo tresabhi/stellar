953debd09692929b12da41c7d14b1910c4239d34
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2021-09-19 19:31:20 -0500

    temporarily disabled workbox

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Workbox from 'workbox-webpack-plugin';
+// import * as Workbox from 'workbox-webpack-plugin';
 
 import App from './App';
 
-Workbox.register();
+// Workbox.register();
 
 ReactDOM.render(
   <React.StrictMode>
