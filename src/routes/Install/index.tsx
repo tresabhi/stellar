f4835705fe102fb8ba3ca5ae6fb78e1e01b26e6d
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2021-09-20 12:31:54 -0500

    added maskable icons

---
+++ src/routes/Install/index.tsx
@@ -0,0 +1,13 @@
+const Install = () => {
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+    window['showInstallPromotion']();
+  });
+
+  return <a href="https://nice.com/">Nice</a>;
+};
+
+export default Install;
