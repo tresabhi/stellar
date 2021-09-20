e229ed68f85aacaa8229653b82e8c813d118281b
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2021-09-19 19:50:56 -0500

    added pre-caching and offline fallbacks to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,14 @@
-import { registerRoute } from 'workbox-routing';
+import { setCatchHandler, registerRoute } from 'workbox-routing';
 import { NetworkFirst, StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
-
-// Used for filtering matches based on status code, header, or both
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-// Used to limit entries in cache, remove entries after a certain period of time
 import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute, matchPrecache } from 'workbox-precaching';
 
-// Cache page navigations (html) with a Network First strategy
 registerRoute(
-  // Check to see if the request is a navigation to a new page
   ({ request }) => request.mode === 'navigate',
-  // Use a Network First caching strategy
   new NetworkFirst({
-    // Put all cached files in a cache named 'pages'
     cacheName: 'pages',
     plugins: [
-      // Ensure that only requests that result in a 200 status are cached
       new CacheableResponsePlugin({
         statuses: [200],
       }),
@@ -23,16 +16,11 @@ registerRoute(
   }),
 );
 
-// Cache CSS, JS, and Web Worker requests with a Stale While Revalidate strategy
 registerRoute(
-  // Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
   ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'worker',
-  // Use a Stale While Revalidate caching strategy
   new StaleWhileRevalidate({
-    // Put all cached files in a cache named 'assets'
     cacheName: 'assets',
     plugins: [
-      // Ensure that only requests that result in a 200 status are cached
       new CacheableResponsePlugin({
         statuses: [200],
       }),
@@ -40,24 +28,27 @@ registerRoute(
   }),
 );
 
-// Cache images with a Cache First strategy
 registerRoute(
-  // Check to see if the request's destination is style for an image
   ({ request }) => request.destination === 'image',
-  // Use a Cache First caching strategy
   new CacheFirst({
-    // Put all cached files in a cache named 'images'
     cacheName: 'images',
     plugins: [
-      // Ensure that only requests that result in a 200 status are cached
       new CacheableResponsePlugin({
         statuses: [200],
       }),
-      // Don't cache more than 50 items, and expire them after 30 days
       new ExpirationPlugin({
         maxEntries: 50,
-        maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+        maxAgeSeconds: 60 * 60 * 24 * 30,
       }),
     ],
   }),
 );
+
+precacheAndRoute(self.__WB_MANIFEST);
+setCatchHandler(async ({ event }) => {
+  if (event.request.destination === 'document') {
+    return matchPrecache('/offline.html');
+  }
+
+  return Response.error();
+});
