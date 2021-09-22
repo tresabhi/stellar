aceda5dbca82481fbe6b9351025fca9a75e130ec
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2021-09-21 20:22:51 -0500

    fixed fuel cell rendering

---
+++ src/parts/FuelTank.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+import Root from './Root';
+import '@react-three/fiber';
+
+export interface Type extends Root {
+  N: {
+    width_original: number;
+    width_a: number;
+    width_b: number;
+    height: number;
+    fuel_percent: number;
+  };
+  T: {
+    color_tex: string;
+    shape_tex: string;
+  };
+}
+
+// ONLY FOR DEBUGGING PURPOSES
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+interface IPart {
+  data: Type;
+}
+export const Part: FC<IPart> = ({ data }) => {
+  return (
+    <mesh position={[data.p.x, data.p.y + data.N.height / 2, 0]}>
+      <cylinderGeometry args={[data.N.width_a / 2, data.N.width_b / 2, data.N.height, 24]} />
+      <meshStandardMaterial roughness={0.2} metalness={0.2} color={getRandomColor()} flatShading={true} />
+    </mesh>
+  );
+
+  // return (
+  //   <mesh position={[data.N.width_original + data.p.x, data.N.height / -2 - data.p.y, 0]}>
+  //     <cylinderGeometry />
+  //     <meshStandardMaterial />
+  //   </mesh>
+  // );
+};
---
+++ src/parts/Root.tsx
@@ -0,0 +1,15 @@
+interface Root {
+  n: string;
+  p: {
+    x: number;
+    y: number;
+  };
+  o: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  t: string;
+}
+
+export default Root;
