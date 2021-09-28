782a2470bbd5424dd813ad76dc9c3938a9b407fc
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2021-09-27 19:51:03 -0500

    fixed all bevel capping issues

---
+++ src/blueprints/fuelTankBenchmark.tsx
@@ -0,0 +1,181 @@
+const color = 'Color_White';
+const shape = 'Edge Faces';
+
+export default Object.assign({
+  center: 9,
+  offset: {
+    x: 0,
+    y: 0,
+  },
+  parts: [
+    {
+      n: 'Fuel Tank',
+      p: {
+        x: 1,
+        y: 0,
+      },
+      o: {
+        x: 1,
+        y: 1,
+        z: 0,
+      },
+      t: '-Infinity',
+      N: {
+        width_original: 2,
+        width_a: 2,
+        width_b: 2,
+        height: 2,
+        fuel_percent: 1,
+      },
+      T: {
+        color_tex: color,
+        shape_tex: shape,
+      },
+    },
+    {
+      n: 'Fuel Tank',
+      p: {
+        x: 4,
+        y: 0,
+      },
+      o: {
+        x: 1,
+        y: 1,
+        z: 0,
+      },
+      t: '-Infinity',
+      N: {
+        width_original: 2,
+        width_a: 2,
+        width_b: 2,
+        height: 4,
+        fuel_percent: 1,
+      },
+      T: {
+        color_tex: color,
+        shape_tex: shape,
+      },
+    },
+    {
+      n: 'Fuel Tank',
+      p: {
+        x: 0,
+        y: 3,
+      },
+      o: {
+        x: 1,
+        y: 1,
+        z: 0,
+      },
+      t: '-Infinity',
+      N: {
+        width_original: 4,
+        width_a: 4,
+        width_b: 4,
+        height: 1,
+        fuel_percent: 1,
+      },
+      T: {
+        color_tex: color,
+        shape_tex: shape,
+      },
+    },
+    {
+      n: 'Fuel Tank',
+      p: {
+        x: -3,
+        y: 0,
+      },
+      o: {
+        x: 1,
+        y: 1,
+        z: 0,
+      },
+      t: '-Infinity',
+      N: {
+        width_original: 4,
+        width_a: 4,
+        width_b: 2,
+        height: 2,
+        fuel_percent: 1,
+      },
+      T: {
+        color_tex: color,
+        shape_tex: shape,
+      },
+    },
+    {
+      n: 'Fuel Tank',
+      p: {
+        x: 0,
+        y: -3,
+      },
+      o: {
+        x: 1,
+        y: 1,
+        z: 0,
+      },
+      t: '-Infinity',
+      N: {
+        width_original: 4,
+        width_a: 1,
+        width_b: 4,
+        height: 2,
+        fuel_percent: 1,
+      },
+      T: {
+        color_tex: color,
+        shape_tex: shape,
+      },
+    },
+    {
+      n: 'Fuel Tank',
+      p: {
+        x: 4,
+        y: -3,
+      },
+      o: {
+        x: 1,
+        y: 1,
+        z: 45,
+      },
+      t: '-Infinity',
+      N: {
+        width_original: 2,
+        width_a: 2,
+        width_b: 2,
+        height: 2,
+        fuel_percent: 1,
+      },
+      T: {
+        color_tex: color,
+        shape_tex: shape,
+      },
+    },
+    {
+      n: 'Fuel Tank',
+      p: {
+        x: -3.5,
+        y: -3,
+      },
+      o: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+      t: '-Infinity',
+      N: {
+        width_original: 1,
+        width_a: 1,
+        width_b: 1,
+        height: 2,
+        fuel_percent: 1,
+      },
+      T: {
+        color_tex: color,
+        shape_tex: shape,
+      },
+    },
+  ],
+  stages: [],
+});
