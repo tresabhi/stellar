4be802e98609a9cf0a9957735aba42e46fa1707a
TresAbhi <coolabhigyaandeep07@gmail.com>
2021-11-05 16:56:45 +0000

    made translator a hook

---
+++ src/core/hooks/useTranslator.tsx
@@ -0,0 +1,15 @@
+export default function (translations: Object, fallback = {}) {
+  return {
+    translate(key: string, variables?: Array<string>) {
+      // TODO: Fix all (object as any) problem
+      let translation: string =
+        (translations as any)[key] ?? (fallback as any)[key] ?? '';
+
+      variables?.forEach((variable, index) => {
+        translation = translation.replaceAll(`{${index}}`, variable);
+      });
+
+      return translation;
+    },
+  };
+}
