227653e92dc7aa55bf794e9f9cbf5f1c355689b9
TresAbhi <coolabhigyaandeep07@gmail.com>
2021-11-11 15:24:55 +0000

    fully implemented splash screen

--- src/components/SplashScreen/index.tsx
+++ src/components/SplashScreen/index.tsx
@@ -1,9 +1,11 @@
 import './index.scss';
 import { ReactComponent as StellarIcon } from 'assets/icons/stellar.svg';
+import { ReactComponent as LoadingIcon } from 'assets/icons/loading.svg';
 
 const SplashScreen = () => (
   <div className="splash-screen">
-    <StellarIcon className="icon" />
+    <StellarIcon className="stellar-icon" />
+    <LoadingIcon className="loading-icon"/>
   </div>
 );
 
