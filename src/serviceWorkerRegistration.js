14cb8e3d5b40e64292728df1a6289ac117051e8b
TresAbhi <coolabhigyaandeep07@gmail.com>
2021-11-12 00:13:37 +0000

    fixed minor equality operator

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -17,7 +17,7 @@ const isLocalhost = Boolean(
     // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/ ||
-        window.location.hostname ==
+        window.location.hostname ===
           'https://3000-white-jaguar-uzykeqbw.ws-us18.gitpod.io',
     ),
 );
