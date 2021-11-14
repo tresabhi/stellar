397290b8c2a0057fa3fc456c0a9170e33d6de006
TresAbhi <coolabhigyaandeep07@gmail.com>
2021-11-14 02:54:12 +0000

    added full support for named builds

--- scripts/build-alpha.js
+++
@@ -1,3 +0,0 @@
-import { exec } from 'child_process';
-
-exec('node ./build.js');
--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,23 @@
-import { exec } from 'child_process';
-import { chdir } from 'process';
+const { exec } = require('child_process');
+const { writeFileSync } = require('fs');
+const { chdir, argv } = require('process');
 
-chdir('../');
-exec('npx react-scripts build');
+console.log('Building with react-scripts');
+// exec('npx react-scripts build');
+
+const appName = 'Stellar';
+const buildNames = {
+  default: '',
+
+  alpha: 'Alpha',
+  beta: 'Beta',
+  release: '',
+};
+const argv2 = argv[2] ?? 'default';
+
+console.log(`Updating manifest.json keys for "${argv2}"`);
+const manifest = require('../build/manifest.json');
+const newName = `${appName} ${buildNames[argv2] ?? ''}`.trim();
+[manifest.name, manifest.short_name] = [newName, newName];
+
+writeFileSync('./build/manifest.json', JSON.stringify(manifest));
