72f5aa532e76d1997c73a231e8d8adf5324fb900
TresAbhi <coolabhigyaandeep07@gmail.com>
2021-11-15 22:07:30 +0000

    modularized manifest key updates

---
+++ modules/updateManifestKeys.js
@@ -0,0 +1,27 @@
+import { writeFileSync } from 'fs';
+import { createRequire } from 'module';
+
+const BUILD_NAMES = {
+  alpha: 'Alpha',
+  beta: 'Beta',
+  release: '',
+};
+
+const require = createRequire(import.meta.url);
+
+export default function renameBuild(buildType, appName) {
+  console.log(`Updating manifest.json keys for "${buildType}"`);
+
+  const manifest = require('../build/manifest.json');
+  const newName = `${appName} ${BUILD_NAMES[buildType] ?? ''}`.trim();
+  writeFileSync(
+    './build/manifest.json',
+    JSON.stringify({
+      ...manifest,
+      name: newName,
+      short_name: newName,
+    }),
+  );
+
+  console.log(`Updating manifest.json keys successful`);
+}
--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,29 @@
-import { exit } from 'process';
+import argsParser from 'args-parser';
+import { argv, exit } from 'process';
 import build from '../modules/build.js';
+import updateManifestKeys from '../modules/updateManifestKeys.js';
 
-// SET TO FALSE WHEN NOT TESTING MODULE
+const APP_NAME = 'Stellar';
+const ARGS = argsParser(argv);
+let BUILD_TYPE;
+
+switch (true) {
+  case ARGS.alpha:
+    BUILD_TYPE = 'alpha';
+    break;
+
+  case ARGS.beta:
+    BUILD_TYPE = 'beta';
+    break;
+
+  default:
+  case ARGS.release:
+    BUILD_TYPE = 'release';
+    break;
+}
+
 build((error) => {
   if (error) exit(1);
 }, true);
+
+updateManifestKeys(BUILD_TYPE, APP_NAME);
