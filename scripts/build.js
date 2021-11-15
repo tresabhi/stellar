f01594ac5212365e0757e03e07bbdbf6f9768182
TresAbhi <coolabhigyaandeep07@gmail.com>
2021-11-15 01:51:59 +0000

    restructured code a bit

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,5 @@
 import { exec } from 'child_process';
-import { createWriteStream } from 'fs';
+import { createWriteStream, existsSync, mkdirSync } from 'fs';
 import { get } from 'https';
 import fetch from 'node-fetch';
 import { argv, exit } from 'process';
@@ -15,6 +15,57 @@ const ICON_IMG = 'https://i.imgur.com/OfK5jC5.png';
 const ARGV2 = argv[2] ?? 'default';
 const FAVICON_API_URL = 'https://realfavicongenerator.net/api/favicon';
 
+// Manifest tweaks based on type of build
+console.log(`Updating manifest.json keys for "${ARGV2}"`);
+const manifest = require('../public/manifest.json');
+const newName = `${APP_NAME} ${BUILD_NAMES[ARGV2] ?? ''}`.trim();
+[manifest.name, manifest.short_name] = [newName, newName];
+writeFileSync('./build/manifest.json', JSON.stringify(manifest));
+console.log(`Updating manifest.json keys successful`);
+
+if (argv[3]) {
+  /**
+   * 1. add all png, svg, ico files
+   * 2. add browserconfig.xml
+   * 3. merge manifest keys
+   */
+  // https://realfavicongenerator.net/api/favicon
+  console.log('Fetching new favicons');
+  const faviconAPIResult = await (
+    await fetch(FAVICON_API_URL, {
+      method: 'post',
+      body: JSON.stringify({
+        favicon_generation: {
+          api_key: argv[3],
+
+          master_picture: { type: 'url', url: ICON_IMG },
+
+          files_location: { type: 'root' },
+        },
+      }),
+    })
+  ).json();
+
+  if (faviconAPIResult.result.status === 'success') {
+    console.log('Fetching successful');
+
+    if (!existsSync('temp')) {
+      console.log(`"temp" directory didn't exist; it was created`);
+      mkdirSync('temp');
+    }
+
+    console.log('Downloading favicons');
+    const faviconZipWriteStream = createWriteStream('temp/favicon.zip');
+    const faviconZipFile = get(
+      faviconAPIResult.favicon_generation_result.favicon.package_url,
+      (response) => {
+        response.pipe(faviconZipWriteStream);
+        console.log(response);
+      },
+    );
+  } else console.error('Fetching failed');
+} else console.warn('No favicon API token provided');
+
 console.log('Building with react-scripts');
 // exec('npx react-scripts build', (error) => {
 exec('echo', async (error) => {
@@ -24,47 +75,4 @@ exec('echo', async (error) => {
   }
 
   console.log('Build succeeded');
-
-  console.log(`Updating manifest.json keys for "${ARGV2}"`);
-  // const manifest = require('../build/manifest.json');
-  const newName = `${APP_NAME} ${BUILD_NAMES[ARGV2] ?? ''}`.trim();
-  // [manifest.name, manifest.short_name] = [newName, newName];
-
-  // NOTE TO SELF: require is relative to current file; node file mutation is
-  // relative to caller
-  // writeFileSync('./build/manifest.json', JSON.stringify(manifest));
-
-  // only if token is provided
-  if (argv[3]) {
-    /**
-     * 1. add all png, svg, ico files
-     * 2. add browserconfig.xml
-     */
-    // https://realfavicongenerator.net/api/favicon
-    const faviconAPIResult = await (
-      await fetch(FAVICON_API_URL, {
-        method: 'post',
-        body: JSON.stringify({
-          favicon_generation: {
-            api_key: argv[3],
-
-            master_picture: { type: 'url', url: ICON_IMG },
-
-            files_location: { type: 'root' },
-          },
-        }),
-      })
-    ).json();
-
-    const faviconZipWriteStream = createWriteStream('temp/favicon.jpg');
-    const faviconZip = get(
-      // faviconAPIResult.favicon_generation_result.favicon.package_url,
-      'https://i.imgur.com/sm4Id90.jpg',
-      (response) => {
-        response.pipe(faviconZipWriteStream);
-      },
-    );
-
-    // console.log();
-  }
 });
