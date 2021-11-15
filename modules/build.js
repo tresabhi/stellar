15f9a0518b2f4aeddcfb0c9ac9382516fb298d8c
TresAbhi <coolabhigyaandeep07@gmail.com>
2021-11-15 18:57:27 +0000

    split up code into modules

---
+++ modules/build.js
@@ -0,0 +1,15 @@
+import { exec } from 'child_process';
+
+export default function build(callback, fakeBuild = false) {
+  console.log('Building with react-scripts');
+
+  exec(fakeBuild ? 'echo' : 'npx react-scripts build', (...args) => {
+    if (args[0]) {
+      console.error(`Build failed; attached error:\n${args[0]}`);
+    } else {
+      console.log('Build succeeded');
+    }
+
+    if (callback) callback(...args);
+  });
+}
