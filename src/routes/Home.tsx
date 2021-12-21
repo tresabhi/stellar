a8e44f9720109aa8abf85fd5e05ca469f2cb0fb0
TresAbhi <coolabhigyaandeep07@gmail.com>
2021-12-21 15:28:45 +0000

    fixed Element[] issue

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,20 +4,27 @@ import * as DevicePicker from 'components/DevicePicker';
 import { isDesktop, isMobile } from 'react-device-detect';
 
 const Home = () => {
-  return [
-    <DevicePicker.Container>
-      <DevicePicker.Title>Which Device Are You On?</DevicePicker.Title>
-      <DevicePicker.List>
-        <DevicePicker.Card recommended={isDesktop} to="/desktop" text="Desktop">
-          <DesktopIcon />
-        </DevicePicker.Card>
-        <DevicePicker.Card recommended={isMobile} to="/mobile" text="Mobile">
-          <MobileIcon />
-        </DevicePicker.Card>
-      </DevicePicker.List>
-      <DevicePicker.Build />
-    </DevicePicker.Container>,
-  ];
+  return (
+    <>
+      <DevicePicker.Container>
+        <DevicePicker.Title>Which Device Are You On?</DevicePicker.Title>
+        <DevicePicker.List>
+          <DevicePicker.Card
+            recommended={isDesktop}
+            to="/desktop"
+            text="Desktop"
+          >
+            <DesktopIcon />
+          </DevicePicker.Card>
+          <DevicePicker.Card recommended={isMobile} to="/mobile" text="Mobile">
+            <MobileIcon />
+          </DevicePicker.Card>
+        </DevicePicker.List>
+        <DevicePicker.Build />
+      </DevicePicker.Container>
+      ,
+    </>
+  );
 };
 
 export default Home;
