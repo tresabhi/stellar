f2f42d2a33e54a08927dc1b28e5459466c8cb44c
TresAbhi <coolabhigyaandeep07@gmail.com>
2021-12-24 00:33:38 +0000

    layed out new themeing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import useStellarName from 'core/hooks/useStellarName';
+import { isMobile } from 'react-device-detect';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Desktop from 'routes/Desktop';
-import Home from 'routes/Home';
 import Mobile from 'routes/Mobile';
 
+import 'styles/index.scss';
+import 'styles/themes/stellark/index.scss';
+
 const Routed = () => {
+  // TODO: use `useNavigate`
+  if (window.location.pathname === '/')
+    window.location.pathname = isMobile ? '/mobile' : '/desktop';
+
   document.title = useStellarName();
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<Home />} />
         <Route path="mobile" element={<Mobile />} />
         <Route path="desktop" element={<Desktop />} />
       </Routes>
