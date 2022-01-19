a7500c49d9bf07ca4af8e54cc4f75a9fbd535045
TresAbhi <coolabhigyaandeep07@gmail.com>
2022-01-19 16:11:04 +0000

    added environments and seperated build and post-build workflows

---
+++ scripts/postBuild.js
@@ -0,0 +1,30 @@
+import argsParser from 'args-parser';
+import { argv, exit } from 'process';
+import generateFavicons from './generateFavicons.js';
+import updateManifestKeys from './updateManifestKeys.js';
+
+const APP_NAME = 'Stellar';
+const ARGS = argsParser(argv);
+let BUILD_TYPE;
+
+switch (true) {
+  case ARGS.alpha:
+    BUILD_TYPE = 'alpha';
+    break;
+
+  case ARGS.beta:
+    BUILD_TYPE = 'beta';
+    break;
+
+  default:
+  case ARGS.release:
+    BUILD_TYPE = 'release';
+    break;
+}
+
+updateManifestKeys(BUILD_TYPE, APP_NAME);
+
+if (ARGS['favicon-api-token'])
+  generateFavicons(BUILD_TYPE, ARGS['favicon-api-token']);
+else console.warn('No favicon API token provided');
+// set to true if you want to use the previous build
