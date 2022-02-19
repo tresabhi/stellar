fa6dc89c9a424b606cf87a5132459b65e0e44e37
CoolAbhi1290 <coolabhigyaandeep07@gmail.com>
2022-02-19 21:59:42 +0000

    fixed duplicate on wheel handlers

--- src/components/DesktopCanvasControls.tsx
+++ src/components/DesktopCanvasControls.tsx
@@ -1,4 +1,5 @@
 import { useThree } from '@react-three/fiber';
+import { useEffect } from 'react';
 import { OrthographicCamera } from 'three';
 import inverseLerp from 'utilities/inverseLerp';
 
@@ -8,38 +9,48 @@ const MIN_ZOOM = 2.2;
 
 const DesktopCanvasControls = () => {
   const canvas = useThree((state) => state.gl.domElement);
-  const camera = useThree((state) => state.camera) as OrthographicCamera;
+  const camera = useThree((state) => state.camera as OrthographicCamera);
   const viewport = useThree((state) => state.viewport);
 
-  canvas.addEventListener('wheel', (event) => {
-    if (event.ctrlKey) {
-      event.preventDefault();
+  useEffect(() => {
+    const handleWheel = (event: WheelEvent) => {
+      if (event.ctrlKey) {
+        event.preventDefault();
 
-      const maxZoom = camera.right - camera.left;
-      const zoomCompensatedDeltaY =
-        event.deltaY * inverseLerp(0, maxZoom, camera.zoom);
-      const zoom = Math.max(
-        MIN_ZOOM,
-        Math.min(maxZoom, camera.zoom - zoomCompensatedDeltaY),
-      );
+        const maxZoom = camera.right - camera.left;
+        const zoomCompensatedDeltaY =
+          event.deltaY * inverseLerp(0, maxZoom, camera.zoom);
+        const zoom = Math.max(
+          MIN_ZOOM,
+          Math.min(maxZoom, camera.zoom - zoomCompensatedDeltaY),
+        );
 
-      camera.zoom = zoom;
-      camera.updateProjectionMatrix();
-    } else {
-      const pixelToWorldUnitRatio =
-        viewport.width / canvas.getBoundingClientRect().width;
-
-      const moveX = (event.deltaX * pixelToWorldUnitRatio * 1.5) / camera.zoom;
-      const moveY = (event.deltaY * pixelToWorldUnitRatio * 1.5) / camera.zoom;
-
-      if (event.shiftKey) {
-        camera.position.x += moveY;
-        camera.position.y -= moveX;
+        camera.zoom = zoom;
+        camera.updateProjectionMatrix();
       } else {
-        camera.position.x += moveX;
-        camera.position.y -= moveY;
+        const pixelToWorldUnitRatio =
+          viewport.width / canvas.getBoundingClientRect().width;
+
+        const moveX =
+          (event.deltaX * pixelToWorldUnitRatio * 1.5) / camera.zoom;
+        const moveY =
+          (event.deltaY * pixelToWorldUnitRatio * 1.5) / camera.zoom;
+
+        if (event.shiftKey) {
+          camera.position.x += moveY;
+          camera.position.y -= moveX;
+        } else {
+          camera.position.x += moveX;
+          camera.position.y -= moveY;
+        }
       }
-    }
+    };
+
+    canvas.addEventListener('wheel', handleWheel);
+
+    return () => {
+      canvas.removeEventListener('wheel', handleWheel);
+    };
   });
 
   return null;
