801295c5e4ae50f7b0396080bbf23e66d4766c0b
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2022-02-24 14:21:27 -0600

    Fix transformation state mix up

--- src/utilities/usePartTransformations.ts
+++ src/utilities/usePartTransformations.ts
@@ -11,12 +11,12 @@ const usePartTransformations = (
   mesh: MutableRefObject<Mesh | Group>,
   offsetHandler?: (state: PartWithTransformations) => number,
 ) => {
-  usePartUpdate(address, initialState, () => {
-    mesh.current.scale.set(initialState.o.x, initialState.o.y, 1);
-    mesh.current.rotation.set(0, 0, degToRad(initialState.o.z));
+  usePartUpdate(address, initialState, (state) => {
+    mesh.current.scale.set(state.o.x, state.o.y, 1);
+    mesh.current.rotation.set(0, 0, degToRad(state.o.z));
     mesh.current.position.set(
-      initialState.p.x,
-      initialState.p.y + (offsetHandler ? offsetHandler(initialState) : 0),
+      state.p.x,
+      state.p.y + (offsetHandler ? offsetHandler(state) : 0),
       0,
     );
   });
