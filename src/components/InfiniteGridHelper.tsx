0005c50cb03c9832d7ec6fe7025969a545421a69
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2022-04-03 20:20:29 -0500

    fixed formatting of infinite grid helper

--- src/components/InfiniteGridHelper.tsx
+++ src/components/InfiniteGridHelper.tsx
@@ -42,43 +42,43 @@ const InfiniteGridHelper = forwardRef<Mesh, InfiniteGridHelperProps>(
               transparent: true,
 
               vertexShader: `
-              varying vec3 worldPosition;
-              uniform float uDistance;
+                varying vec3 worldPosition;
+                uniform float uDistance;
 
-              void main() {
-                vec3 pos = position.${axes} * uDistance;
-                pos.${planeAxes} += cameraPosition.${planeAxes};
-                worldPosition = pos;
-                gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
-              }
-            `,
+                void main() {
+                  vec3 pos = position.${axes} * uDistance;
+                  pos.${planeAxes} += cameraPosition.${planeAxes};
+                  worldPosition = pos;
+                  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+                }
+              `,
 
               fragmentShader: `
-              varying vec3 worldPosition;
-              uniform float uSize1;
-              uniform float uSize2;
-              uniform vec3 uColor;
-              uniform float uDistance;
+                varying vec3 worldPosition;
+                uniform float uSize1;
+                uniform float uSize2;
+                uniform vec3 uColor;
+                uniform float uDistance;
 
-              float getGrid(float size) {
-                vec2 r = worldPosition.${planeAxes} / size;
-                vec2 grid = abs(fract(r - 0.5) - 0.5) / fwidth(r * 1.5);
-                float line = min(grid.x, grid.y);
+                float getGrid(float size) {
+                  vec2 r = worldPosition.${planeAxes} / size;
+                  vec2 grid = abs(fract(r - 0.5) - 0.5) / fwidth(r * 1.5);
+                  float line = min(grid.x, grid.y);
 
-                return 1.0 - min(line, 1.0);
-              }
+                  return 1.0 - min(line, 1.0);
+                }
 
-              void main() {
-                float d = 1.0;
-                float g1 = getGrid(uSize1);
-                float g2 = getGrid(uSize2);
+                void main() {
+                  float d = 1.0;
+                  float g1 = getGrid(uSize1);
+                  float g2 = getGrid(uSize2);
 
-                gl_FragColor = vec4(uColor.rgb, mix(g2, g1, g1));
-                gl_FragColor.a = mix(0.5 * gl_FragColor.a, gl_FragColor.a, g2);
+                  gl_FragColor = vec4(uColor.rgb, mix(g2, g1, g1));
+                  gl_FragColor.a = mix(0.5 * gl_FragColor.a, gl_FragColor.a, g2);
 
-                if ( gl_FragColor.a <= 0.0 ) discard;
-              }
-            `,
+                  if ( gl_FragColor.a <= 0.0 ) discard;
+                }
+              `,
             },
           ]}
         />
