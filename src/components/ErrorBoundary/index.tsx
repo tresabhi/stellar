95567a519bc307e43b6559f7bb80c4d2f202fb54
CoolAbhi1290 <coolabhigyaandeep07@gmail.com>
2022-04-06 00:41:52 +0000

    added a lot of logs to report button

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,7 +1,9 @@
 import { ReactComponent } from 'assets/icons/warning-yellow.svg';
 import Button from 'components/Button';
 import TextArea from 'components/TextArea';
+import moment from 'moment';
 import { FC, useState } from 'react';
+import { deviceDetect } from 'react-device-detect';
 import {
   ErrorBoundary as ErrorBoundaryLib,
   FallbackProps,
@@ -9,15 +11,37 @@ import {
 import blueprintStore from 'stores/blueprint';
 import styles from './index.module.scss';
 
+const MESSAGE_MAX_LENGTH = 75;
+
 export const ErrorBoundaryFallback: FC<FallbackProps> = ({
   error,
   resetErrorBoundary,
 }) => {
   const [logVisible, setLogVisible] = useState(false);
   const hasUnsavedChanges = blueprintStore((state) => state.hasUnsavedChanges);
-
+  const slicedMessage = error.message.slice(0, MESSAGE_MAX_LENGTH - 1);
+  const ellipses = error.message.length > MESSAGE_MAX_LENGTH ? '...' : '';
+  const deviceInfo = {
+    format_version: 1,
+    hasUnsavedChanges,
+    locale: moment.locale(),
+    date: moment().format('MMMM Do, YYYY'),
+    time: moment().format('h:mm:ss a'),
+    ...deviceDetect(undefined),
+  };
+  const deviceInfoStringified = Object.keys(deviceInfo)
+    .map((key) => {
+      const value = deviceInfo[key];
+      return `${key}: ${value}`;
+    })
+    .join('\n');
   const URLParams = new URLSearchParams({
-    title: `[Bug] ${error.name}`,
+    title: `[Bug] ${error.name}: ${slicedMessage}${ellipses}`,
+    body: `## About\n<REPLACE ME WITH INFORMATION ABOUT WHAT HAPPENED>\n\n## Reproduce\n<REPLACE ME WITH INFORMATION ON HOW TO REPRODUCE THIS ISSUE>\n\n## Error\n\`\`\`\n${
+      error.message
+    }\n\`\`\`${
+      error.stack ? `\n\n## Stack\n\`\`\`\n${error.stack}\n\`\`\`` : ''
+    }\n\n## Other Info\n\`\`\`\n${deviceInfoStringified}\n\`\`\``,
   }).toString();
   const reportURL = `https://github.com/TresAbhi/Stellar/issues/new?${URLParams}`;
 
@@ -35,9 +59,13 @@ export const ErrorBoundaryFallback: FC<FallbackProps> = ({
       </span>
       {logVisible ? (
         [
-          <TextArea key="error-message">{error.message}</TextArea>,
+          <TextArea className={styles.debug} key="error-message">
+            {error.message}
+          </TextArea>,
           error.stack ? (
-            <TextArea key="error-stack">{error.stack}</TextArea>
+            <TextArea className={styles.debug} key="error-stack">
+              {error.stack}
+            </TextArea>
           ) : null,
         ]
       ) : (
