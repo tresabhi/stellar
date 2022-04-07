27e4e082f6efde916db7c8f59e85f8f4ab520aa3
CoolAbhi1290 <coolabhigyaandeep07@gmail.com>
2022-04-07 17:59:14 +0000

    added more interactions for the scrollbar

--- src/components/Scrollable/index.tsx
+++ src/components/Scrollable/index.tsx
@@ -3,15 +3,25 @@ import styles from './index.module.scss';
 
 const PADDING = 4;
 const THUMB_MIN_HEIGHT = 64;
+const INACTIVE_TIME = 2000;
 
 const Scrollable: FC<InputHTMLAttributes<HTMLDivElement>> = ({
   children,
   className,
   ...props
 }) => {
+  let timeout: NodeJS.Timeout | undefined;
   const containerRef = useRef<HTMLDivElement>(null!);
   const thumbRef = useRef<HTMLDivElement>(null!);
 
+  const markThumbAsActive = () => {
+    thumbRef.current.classList.remove(styles.inactive);
+
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      thumbRef.current.classList.add(styles.inactive);
+    }, INACTIVE_TIME);
+  };
   const updateThumb = () => {
     const containerHeight = containerRef.current.getBoundingClientRect().height;
 
@@ -30,16 +40,21 @@ const Scrollable: FC<InputHTMLAttributes<HTMLDivElement>> = ({
 
     thumbRef.current.style.top = `${offset}px`;
   };
+  const handleScroll = () => {
+    updateThumb();
+    markThumbAsActive();
+  };
 
   useEffect(updateThumb);
 
   return (
     <div {...props} className={`${styles.scrollable} ${className ?? ''}`}>
-      <div className={styles.thumb} ref={thumbRef} />
+      <div className={`${styles.thumb} ${styles.inactive}`} ref={thumbRef} />
       <div
         className={styles.container}
         ref={containerRef}
-        onScroll={updateThumb}
+        onScroll={handleScroll}
+        onMouseOver={markThumbAsActive}
       >
         {children}
       </div>
