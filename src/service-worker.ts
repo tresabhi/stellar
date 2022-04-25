2ffa5c7ea8eadf1670283555aae6f7c60acd5dbf
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2022-04-25 11:01:15 -0500

    added WorkerGlobalScope

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -6,7 +6,7 @@ import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
 
-declare const self: ServiceWorkerGlobalScope;
+declare const self: ServiceWorkerGlobalScope & WorkerGlobalScope;
 
 clientsClaim();
 precacheAndRoute(self.__WB_MANIFEST);
