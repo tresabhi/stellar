0e0fe562c9c0116ab0d2965545cc2b0b39441b2c
CoolAbhi1290 <coolabhigyaandeep07@gmail.com>
2022-05-17 23:18:34 +0000

    fixed missing translation files

--- src/hooks/useTranslator.ts
+++ src/hooks/useTranslator.ts
@@ -1,15 +1,44 @@
 import { AnyObject } from 'immer/dist/internal';
+import de_DE from 'lang/de-DE.json';
+import en_GB from 'lang/en-GB.json';
+import en_US from 'lang/en-US.json';
+import es_ES from 'lang/es-ES.json';
+import fr_FR from 'lang/fr-FR.json';
+import ga_IE from 'lang/ga-IE.json';
+import hi_IN from 'lang/hi-IN.json';
+import it_IT from 'lang/it-IT.json';
+import ja_JP from 'lang/ja-JP.json';
+import ko_KR from 'lang/ko-KR.json';
+import ru_RU from 'lang/ru-RU.json';
+import sv_SE from 'lang/sv-SE.json';
+import zh_CN from 'lang/zh-CN.json';
+import zh_TW from 'lang/zh-TW.json';
 import { isUndefined } from 'lodash';
 import useSettings from './useSettings';
 
+export const translationsRegistry = new Map<string, AnyObject>([
+  ['de-DE', de_DE],
+  ['en-GB', en_GB],
+  ['en-US', en_US],
+  ['es-ES', es_ES],
+  ['fr-FR', fr_FR],
+  ['ga-IE', ga_IE],
+  ['hi-IN', hi_IN],
+  ['it-IT', it_IT],
+  ['ja-JP', ja_JP],
+  ['ko-KR', ko_KR],
+  ['ru-RU', ru_RU],
+  ['sv-SE', sv_SE],
+  ['zh-CN', zh_CN],
+  ['zh-TW', zh_TW],
+]);
+
 const useTranslator = () => {
   const language = useSettings((state) => state.language);
-  let translations = {};
+  let translations = translationsRegistry.get(language);
 
-  try {
-    translations = require(`lang/${language}.json`);
-  } catch {
-    console.error(`Language ${language} not found`);
+  if (isUndefined(translations)) {
+    console.warn(`No translations for language ${language}`);
   }
 
   const translate = (string: string) => {
