495afe288da5515dfc375c01635c387d80e17487
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2022-08-16 12:15:08 +0530

    feat: added more basic templates

--- src/routes/components/Interface/components/TabCreate.tsx
+++ src/routes/components/Interface/components/TabCreate.tsx
@@ -1,15 +1,13 @@
-import { FilePlusIcon, FileTextIcon } from '@radix-ui/react-icons';
-import dragonHelicopter from 'assets/blueprints/dragon-helicopter.json';
-import gaganyaan from 'assets/blueprints/gaganyaan.json';
-import marsVenusRover from 'assets/blueprints/mars-venus-rover.json';
-import notNationalTeamLander from 'assets/blueprints/not-national-team-lander.json';
-import perseveranceRover from 'assets/blueprints/perseverance-rover.json';
-import saturnV from 'assets/blueprints/saturn-v.json';
-import smallShuttle from 'assets/blueprints/small-shuttle.json';
-import sovietN1 from 'assets/blueprints/soviet-n1.json';
-import starHauler from 'assets/blueprints/star-hauler.json';
-import starship from 'assets/blueprints/starship.json';
-import titanIIIC from 'assets/blueprints/titan-iiic.json';
+import { ArchiveIcon, FileTextIcon } from '@radix-ui/react-icons';
+import apolloMission from 'assets/blueprints/apollo-mission.json';
+import basicRocket from 'assets/blueprints/basic-rocket.json';
+import hopper from 'assets/blueprints/hopper.json';
+import joints from 'assets/blueprints/joints.json';
+import lightLander from 'assets/blueprints/light-lander.json';
+import orbitAndReEntry from 'assets/blueprints/orbit-and-re-entry.json';
+import parachuteAndDecoupler from 'assets/blueprints/parachute-and-decoupler.json';
+import raceCar from 'assets/blueprints/race-car.json';
+import rover from 'assets/blueprints/rover.json';
 import { Pallet, PalletItem } from 'components/Pallet';
 import { loadBlueprint } from 'core/blueprint';
 import { VanillaBlueprint } from 'game/Blueprint';
@@ -17,34 +15,100 @@ import { styled, theme } from 'stitches.config';
 import useApp, { TAB } from 'stores/useApp';
 import { TabContainer } from './Tabs';
 
-const TEMPLATES: [string, string, number, VanillaBlueprint, boolean][] = [
-  ['Saturn V', 'Luke96', 4805, saturnV, true],
-  ['Starship', 'SouthwestSpaceFlight', 4673, starship, true],
-  ['Soviet N1', 'SSA', 5649, sovietN1, true],
-  ['Perseverance Rover', 'alexmb48', 4829, perseveranceRover, false],
-  ['Gaganyaan', 'SFSAbhishek', 3320, gaganyaan, true],
-  ['Titan IIIC', 'Cjuwa1985', 4688, titanIIIC, true],
-  ['Mars/Venus Rover', 'Bilo92', 6120, marsVenusRover, true],
-  ['Small Shuttle', 'Soyuzturtle', 4706, smallShuttle, false],
-  ['Dragon Helicopter', 'mosscow', 4845, dragonHelicopter, false],
-  ['Star Hauler', 'Sheepy', 5528, starHauler, true],
-  ["National Team's Lander", 'Kaen', 6007, notNationalTeamLander, false],
+const ABHI = 'TrèsAbhi';
+const ABHI_WEB = 'https://tresabhi.github.io/';
+
+interface Template {
+  name: string;
+  author: string;
+  link: number | string;
+  blueprint: VanillaBlueprint;
+  inbuilt: boolean;
+}
+
+const TEMPLATES: Template[] = [
+  {
+    name: 'Hopper',
+    author: ABHI,
+    link: ABHI_WEB,
+    blueprint: hopper,
+    inbuilt: true,
+  },
+  {
+    name: 'Apollo Mission',
+    author: ABHI,
+    link: ABHI_WEB,
+    blueprint: apolloMission,
+    inbuilt: true,
+  },
+  {
+    name: 'Light Lander',
+    author: ABHI,
+    link: ABHI_WEB,
+    blueprint: lightLander,
+    inbuilt: true,
+  },
+  {
+    name: 'Rover',
+    author: ABHI,
+    link: ABHI_WEB,
+    blueprint: rover,
+    inbuilt: true,
+  },
+  {
+    name: 'Parachute & Decoupler',
+    author: ABHI,
+    link: ABHI_WEB,
+    blueprint: parachuteAndDecoupler,
+    inbuilt: true,
+  },
+  {
+    name: 'Joints',
+    author: ABHI,
+    link: ABHI_WEB,
+    blueprint: joints,
+    inbuilt: true,
+  },
+  {
+    name: 'Orbit & Re-entry',
+    author: ABHI,
+    link: ABHI_WEB,
+    blueprint: orbitAndReEntry,
+    inbuilt: true,
+  },
+  {
+    name: 'Race Car',
+    author: ABHI,
+    link: ABHI_WEB,
+    blueprint: raceCar,
+    inbuilt: true,
+  },
+  {
+    name: 'Basic Rocket',
+    author: ABHI,
+    link: ABHI_WEB,
+    blueprint: basicRocket,
+    inbuilt: true,
+  },
 ];
 const PALLET_ITEMS = TEMPLATES.sort((a, b) => {
-  if (a[0] < b[0]) return -1;
-  if (a[0] > b[0]) return 1;
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
   return 0;
 }).map(
   (template) =>
     ({
-      name: template[0],
-      note: template[1],
-      noteURL: `https://jmnet.one/sfs/forum/index.php?members/${template[2]}`,
+      name: template.name,
+      note: template.author,
+      noteURL:
+        typeof template.link === 'string'
+          ? template.link
+          : `https://jmnet.one/sfs/forum/index.php?members/${template.link}`,
       callback: () => {
-        loadBlueprint(template[3]);
+        loadBlueprint(template.blueprint);
         useApp.setState({ tab: TAB.LAYOUT });
       },
-      icon: template[4] ? <FilePlusIcon /> : <FileTextIcon />,
+      icon: template.inbuilt ? <FileTextIcon /> : <ArchiveIcon />,
     } as PalletItem),
 );
 
