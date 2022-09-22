f720432f064513e681889c7da1382cbabc12aabc
tresabhi <coolabhigyaandeep07@gmail.com>
2022-09-22 15:03:05 +0000

    feat: migrated to vite

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgrPlugin from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  build: { outDir: 'build' },
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    svgrPlugin({
+      svgrOptions: { icon: true },
+    }),
+  ],
+});
