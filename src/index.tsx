16a55ba09776cee2ccc12d2b056326ff632e442c
tresabhi <coolabhigyaandeep07@gmail.com>
2022-09-23 15:22:21 +0000

    perf: lazy-loaded preroot

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import { lazy, StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import SplashScreen from 'routes/components/SplashScreen';
-import preroot from 'utilities/preroot';
 
-preroot();
+import('utilities/preroot').then((value) => {
+  value.default();
+});
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
