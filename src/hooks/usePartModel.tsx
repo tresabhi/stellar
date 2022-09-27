2f4bbe74a7faeeeaa7128afbf225828495cbd8df
tresabhi <coolabhigyaandeep07@gmail.com>
2022-09-27 15:57:21 +0000

    fix: corrected the usage of `GLTFResult`

--- src/hooks/usePartModel.tsx
+++ src/hooks/usePartModel.tsx
@@ -7,7 +7,9 @@ import GLTFResult from 'types/GLTFResult';
 export const PART_MODEL_PATH = '/assets/models/parts/';
 
 const usePartModel = (fileName: string, modelName: string) => {
-  const result = useGLTF(`${PART_MODEL_PATH}${fileName}.gltf`) as GLTFResult;
+  const result = useGLTF(
+    `${PART_MODEL_PATH}${fileName}.gltf`,
+  ) as unknown as GLTFResult;
 
   const Component = forwardRef<Mesh, MeshProps>((props, ref) => (
     <mesh {...props} ref={ref} geometry={result.nodes[modelName].geometry}>
