86e16451591c7251becd6c07ed5e92b9088cbe95
tresabhi <coolabhigyaandeep07@gmail.com>
2022-10-16 19:36:24 +0000

    fix: invalidated frame on undo/redo

--- src/core/blueprint/redoVersion.ts
+++ src/core/blueprint/redoVersion.ts
@@ -1,5 +1,6 @@
 import { mutateVersionControl } from 'core/app';
 import { applyPatches } from 'immer';
+import useApp from 'stores/useApp';
 import useBlueprint from 'stores/useBlueprint';
 import { UseVersionControl } from 'stores/useVersionControl';
 import { declareUnsavedChanges } from './declareUnsavedChanges';
@@ -7,12 +8,14 @@ import { declareUnsavedChanges } from './declareUnsavedChanges';
 export const redoVersion = () => {
   mutateVersionControl((draft: UseVersionControl) => {
     const patches = draft.history[draft.index + 1]?.patches;
+    const { invalidateFrame } = useApp.getState().editor;
 
     if (patches) {
       useBlueprint.setState(applyPatches(useBlueprint.getState(), patches));
     }
 
     draft.index = Math.min(draft.history.length - 1, draft.index + 1);
+    invalidateFrame && invalidateFrame();
   });
 
   declareUnsavedChanges();
--- src/core/blueprint/undoVersion.ts
+++ src/core/blueprint/undoVersion.ts
@@ -1,5 +1,6 @@
 import { mutateVersionControl } from 'core/app';
 import { applyPatches } from 'immer';
+import useApp from 'stores/useApp';
 import useBlueprint from 'stores/useBlueprint';
 import { UseVersionControl } from 'stores/useVersionControl';
 import { declareUnsavedChanges } from './declareUnsavedChanges';
@@ -7,6 +8,7 @@ import { declareUnsavedChanges } from './declareUnsavedChanges';
 export const undoVersion = () => {
   mutateVersionControl((draft: UseVersionControl) => {
     const inversePatches = draft.history[draft.index]?.inversePatches;
+    const { invalidateFrame } = useApp.getState().editor;
 
     if (inversePatches) {
       useBlueprint.setState(
@@ -15,6 +17,7 @@ export const undoVersion = () => {
     }
 
     draft.index = Math.max(-1, draft.index - 1);
+    invalidateFrame && invalidateFrame();
   });
 
   declareUnsavedChanges();
