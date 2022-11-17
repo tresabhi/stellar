29e1bed323c73948ba9827c1ee324a87c81138ec
tresabhi <coolabhigyaandeep07@gmail.com>
2022-11-17 00:16:59 +0000

    fix: sole part selection creating unnecessary version history

--- src/core/part/selectPartOnly.ts
+++ src/core/part/selectPartOnly.ts
@@ -1,6 +1,7 @@
 import { Blueprint } from 'game/Blueprint';
+import { getPart } from './getPart';
 import { selectPartsOnly } from './selectPartsOnly';
 
 export const selectPartOnly = (id: string, draft?: Blueprint) => {
-  selectPartsOnly([id], draft);
+  if (!getPart(id).selected) selectPartsOnly([id], draft);
 };
