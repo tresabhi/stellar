3dc27526f981c5ae84e747c8eb970202cdd03aca
tresabhi <coolabhigyaandeep07@gmail.com>
2022-12-27 03:52:31 +0000

    fix: `OrthographicCamera` type expose typing issue

--- src/components/LayoutCanvas/components/Expose.tsx
+++ src/components/LayoutCanvas/components/Expose.tsx
@@ -1,12 +1,13 @@
 import { useThree } from '@react-three/fiber';
 import mutateApp from 'core/app/mutateApp';
+import { OrthographicCamera } from 'three';
 
 export default function Expose() {
   const camera = useThree((state) => state.camera);
   const canvas = useThree((state) => state.gl.domElement);
 
   mutateApp((draft) => {
-    draft.editor.camera = camera;
+    draft.editor.camera = camera as OrthographicCamera;
     draft.editor.canvas = canvas;
   });
 
