2e3c9d0d74e0d6d156d2e0c512020ec98a9f6bee
tresabhi <coolabhigyaandeep07@gmail.com>
2023-01-20 01:11:19 +0000

    fix: a few compile errors

--- src/core/blueprint/loadBlueprintTemplate.ts
+++ src/core/blueprint/loadBlueprintTemplate.ts
@@ -1,13 +1,9 @@
-import onePart from 'assets/blueprints/one-part.json';
 import shapeAndTextures1 from 'assets/blueprints/shape-and-textures-1.json';
-import testFuelTank from 'assets/blueprints/test-fuel-tank.json';
 import { VanillaBlueprint } from 'game/Blueprint';
 import loadBlueprint from './loadBlueprint';
 
 export const templateBlueprints: { [key: string]: VanillaBlueprint } = {
-  testFuelTank,
   shapeAndTextures1,
-  onePart,
 };
 
 export default function loadBlueprintTemplate(name?: string) {
