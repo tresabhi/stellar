a7920beb658407416be5587df20f1ac3a8fb2905
tresabhi <coolabhigyaandeep07@gmail.com>
2023-03-10 01:31:17 +0000

    feat: probe

---
+++ src/game/parts/Probe/index.tsx
@@ -0,0 +1,46 @@
+import { ReactComponent as ProbeIconPrimitive } from 'assets/icons/probe.svg';
+import PartCategory from 'hooks/constants/partCategory';
+import { PartRegistryItem } from 'stores/partRegistry';
+import createPhysicalPart from 'utilities/createPhysicalPart';
+import { PartData, PartWithoutName } from '../Part';
+import {
+  VanillaPartWithTransformations,
+  VanillaPartWithTransformationsData,
+} from '../PartWithTransformations';
+import model from './model.gltf';
+
+export interface VanillaProbe extends VanillaPartWithTransformations {
+  readonly n: 'Probe';
+  N: { width: number };
+}
+
+export interface Probe extends PartWithoutName, VanillaProbe {}
+
+export const VanillaProbeData: VanillaProbe = {
+  ...VanillaPartWithTransformationsData,
+
+  n: 'Probe',
+  N: { width: 1 },
+};
+
+export const ProbeData: Probe = {
+  ...PartData,
+  ...VanillaProbeData,
+
+  label: 'Probe',
+};
+
+export const ProbeLayoutComponent = createPhysicalPart(model);
+
+export const ProbeIcon = ProbeIconPrimitive;
+
+export const registry: PartRegistryItem<Probe> = {
+  category: PartCategory.Structural,
+  vanillaData: VanillaProbeData,
+  data: ProbeData,
+
+  Icon: ProbeIcon,
+  Mesh: ProbeLayoutComponent,
+
+  exportify: undefined,
+};
