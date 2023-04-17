d43151d2a36df735e5d35ca5be566fc309e95720
tresabhi <coolabhigyaandeep07@gmail.com>
2023-04-17 00:46:05 +0000

    feat: parts rendering on the left side bar

--- src/routes/Interface/components/StagingTab/components/LeftSidebar/index.tsx
+++ src/routes/Interface/components/StagingTab/components/LeftSidebar/index.tsx
@@ -1,12 +1,14 @@
 import * as Sidebar from 'components/Sidebar';
 import mutateSettings from 'core/app/mutateSettings';
-import useSettings from 'stores/settings';
+import useTouchscreenMode from 'hooks/useTouchscreenMode';
+import Parts from 'routes/Interface/components/LayoutTab/components/LeftSidebar/components/Parts';
+import useSettings, { SidebarTab } from 'stores/settings';
 import Stages from './components/Stages';
 import Tabs from './components/Tabs';
 
 export default function LeftSidebar() {
-  const visible = useSettings(
-    (state) => state.interface.tabs.staging.leftSidebar.visible,
+  const { visible, tab } = useSettings(
+    (state) => state.interface.tabs.staging.leftSidebar,
   );
   const handleCollapseClick = () => {
     mutateSettings((draft) => {
@@ -14,12 +16,13 @@ export default function LeftSidebar() {
       leftSidebar.visible = !leftSidebar.visible;
     });
   };
+  const touchscreenMode = useTouchscreenMode();
 
   return (
     <Sidebar.Root visible={visible} position="left">
       <Tabs />
 
-      <Stages />
+      {tab === SidebarTab.Left || !touchscreenMode ? <Stages /> : <Parts />}
 
       <Sidebar.Collapse
         position="left"
