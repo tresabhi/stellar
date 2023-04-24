d12eb32c982f2f2755231e6b4ffe94ca6b987b14
tresabhi <coolabhigyaandeep07@gmail.com>
2023-04-24 14:18:19 +0000

    feat: removed dependencies on splash screen

--- src/routes/components/SplashScreen/index.tsx
+++ src/routes/components/SplashScreen/index.tsx
@@ -1,36 +1,28 @@
 import { BarLoader } from 'react-spinners';
-import { styled, theme } from 'stitches.config';
-import themeDark from 'stitches.config/themes/dark';
 import getContext from 'utilities/getContext';
-
-const Container = styled('div', {
-  backgroundColor: theme.colors.appBackground1,
-  height: '100vh',
-  width: '100vw',
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  gap: theme.space.gapUnrelatedMajor,
-
-  '& > svg': {
-    width: theme.sizes[64],
-    height: theme.sizes[64],
-  },
-});
 
 function SplashScreen() {
   const { Icon } = getContext();
 
   return (
-    <Container className={themeDark}>
-      <Icon />
-
-      <BarLoader color={theme.colors.solidBackground_accent.toString()} />
-    </Container>
+    <div
+      style={{
+        backgroundColor: 'hsl(246, 6.0%, 9.0%)',
+        height: '100vh',
+        width: '100vw',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '1rem',
+      }}
+    >
+      <Icon style={{ width: '4rem', height: '4rem' }} />
+      <BarLoader color="hsl(275, 80.0%, 71.0%)" />
+    </div>
   );
 }
 
